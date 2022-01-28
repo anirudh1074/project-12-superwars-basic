299a3ee2d0117cd49147086658cac8c774b66ffe
KANAMRLAPUDI NVSP ANURUDH <anirudh0116@gmail.com>
2022-01-28 12:17:51 +0530

    superwars stage2

--- src/app.js
+++ src/app.js
@@ -26,7 +26,15 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    for (var i=0; i<players.length; i++) {
+        
+        detailedPlayers[i] = {
+            name:players[i],
+            strength:getRandomStrength(),
+            image:"images/super-"+(i+1)+".png",
+            type:(i % 2 != 0) ?"hero":"villain"
+        }
+    }
     return detailedPlayers;
 }
 
@@ -34,6 +42,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random()*100);
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +51,17 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for( var i=0; i<players.length; i++) {
+        single_player = `<div class="player">
+        <img src="${players[i].image}" alt="">
+        <div class="name">${players[i].name}</div>
+        <div class="strength">${players[i].strength}</div>
+        </div>
+        `;
+        if(players[i].type==type){
+            fragment=fragment+single_player;
+        }
+    }
 
     return fragment;
 }
@@ -53,6 +73,4 @@ const viewPlayers = (players) => {
 
 }
 
-window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+window.onload = () => {viewPlayers(initPlayers(PLAYERS));}
